33865adf0e2b7ed8299de85d6d0738bf0f9ca62a
swand0g <alexanderjswanson@icloud.com>
2022-07-01 17:04:43 -0400

    basic script run in api route, tidy module

--- server.go
+++ server.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type Handler = func(c *fiber.Ctx) error
 type FiberContext = *fiber.Ctx
 
-func getTests(c FiberContext) error {
-	return c.SendString("This will get all tests from a queue!")
-}
-
-func getTestById(c FiberContext) error {
-	return c.SendString("This will get a test by the ID in the route: " + c.Params("id") + "!")
+type Ko6aTest struct {
+	id 					string		// The id of the test run
+	scriptUrl 	string 		// The github url of the script to run
+	testResult  string 		// Result output of the test
 }
 
 func runTest(c *fiber.Ctx) error {
+	RunScript(Ko6aTest{
+		id: "1",
+		scriptUrl: "https://github.com/beansource/stinky-mud-puddle/blob/main/using-k6/03-metrics.js",
+	})
+	fmt.Println("Ran test")
 	return c.SendString("This will add a test to a queue, and return a job id!")
 }
 
@@ -30,9 +35,11 @@ func main() {
 	})
 
 	tests := app.Group("/tests")
-	tests.Get("/", getTests)
-	tests.Get("/:id", getTestById)
-	tests.Post("/run", runTest)
+	tests.Get("/run", runTest)
+
+	// go func() {
+	// 	ksix.RunScript()
+	// }()
 
 	app.Listen(":9090")
 }
